80171822c17e3928a585c262cd4ee5ce95cb33bc
Adebayo Victor <adebayovolanrewaju@yahoo.com>
2024-07-09 09:51:13 -0400

    continuous integration concepts [ESLint, Matrix]

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    files: ["**/*.js"],
+    extends: ["eslint:recommended"],
+    rules: {
+      "indent": ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      "quotes": ["error", "single"],
+      "semi": ["error", "always"],
+      // additional, custom rules here
+    },
+    env: {
+      "node": true,
+      "es6": true,
+      "jest": true  // If you are using Jest for testing
+    }
+  }
+];
